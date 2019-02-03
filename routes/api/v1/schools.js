09cb8a480d0b96ff7327378d9e5fa70df101c65c
rishimehta365 <rishimehta365@gmail.com>
2019-02-03 12:18:30 +0530

    Redis Caching with Pagination
    Added Redis Caching

--- routes/api/v1/schools.js
+++ routes/api/v1/schools.js
@@ -1,14 +1,20 @@
-
 const mongoose = require('mongoose'),
 router = require('express').Router(),
-auth = require('../../auth');
+auth = require('../../auth'),
+ttl= 60 * 60 * 1,
+axios = require('axios'),
+redis = require('redis');
 
 var Schools = require('../../../models/Schools'),
     url= require('url');
 
-    
-   
-    
+const client = redis.createClient();
+
+client.on('error', (err)=> {
+    console.log('Error: '+err);
+});
+
+     
 
 router.post('/', auth.optional,(req, res, next)=>{
     
@@ -22,18 +28,33 @@ router.post('/', auth.optional,(req, res, next)=>{
 router.get('/', auth.optional, (req, res, next) => {
 
     var get_params = url.parse(req.url, true).query;
-
+  
     if(get_params['limit']!=null || get_params['page']!=null){
-        return Schools.paginate({}, Schools.find({}).limit(req.query.limit).skip(req.skip).lean().exec().then((schools)=>{
-                return res.json({
-                    object: 'schools',
-                    page_count: req.query.page,
-                    result: schools
-                });
-            }).catch(err=> next(err)));
+        
+        return client.get('/api/v1/schools?page='+req.query.page+'&limit='+req.query.limit,(err,result)=>{
+            if(result){
+                console.log('/api/v1/schools?page='+req.query.page+'&limit='+req.query.limit);
+                const resultJSON = JSON.parse(result);
+                return res.status(200).json(resultJSON);
+            }
+            else
+            {
+                return axios.get(
+                    Schools.paginate({}, Schools.find().limit(req.query.limit).skip(req.skip).lean().exec().then((schools)=>{
+                        const responseJSON = schools;
+                    client.setex('/api/v1/schools?page='+req.query.page+'&limit='+req.query.limit, 3600, JSON.stringify({ source: '_cache_get_scrank_all', ...responseJSON, }));
+           
+                        return res.status(200).json({
+                            object: 'schools',
+                            page_count: req.query.page,
+                            result: schools
+                        });
+                    }).catch(err =>{
+                        next(err);
+                    })));    
+            }
+        });
     }
-   
-    
 });
 
 module.exports = router;
